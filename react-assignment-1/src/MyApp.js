8ec8598d3aed8b7bd022e24af542df5db4cfbbb6
Mike Preis <mikepreis@users.noreply.github.com>
2021-01-16 15:48:16 -0800

    Added index.css and MyApp.js

---
+++ react-assignment-1/src/MyApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MyApp() {
+    return ( 
+      <div> 
+         <h1>Hello, React!</h1> 
+      </div> 
+    ); 
+}
+
+// This makes the component available to be imported to other files
+export default MyApp;
